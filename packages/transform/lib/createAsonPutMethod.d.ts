254cfe92ba67cc789b8c8a66f6684c5fa49c80ab
Joshua Tenner <tenner.joshua@gmail.com>
2021-04-27 23:48:16 -0400

    update packages, remove lib

--- packages/transform/lib/createAsonPutMethod.d.ts
+++
@@ -1,3 +0,0 @@
-import { ClassDeclaration } from "visitor-as/as";
-export declare function createAsonPutMethod(classDeclaration: ClassDeclaration): void;
-//# sourceMappingURL=createAsonPutMethod.d.ts.map
